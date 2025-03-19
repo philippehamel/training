58ae8cca11472a04551ea4474a31aadc005bdf33
Philippe Hamel <philippehamel85@gmail.com>
2025-03-18 23:49:05 -0400

    assignment 2

---
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/model/Publisher.java
@@ -0,0 +1,60 @@
+package guru.springframework.spring5webapp.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Publisher {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+
+    public Publisher(String name, String address, String city, String state, String zip) {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        Publisher publisher = (Publisher) o;
+        return Objects.equals(getId(), publisher.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                '}';
+    }
+}
