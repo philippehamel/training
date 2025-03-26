15decde8bdc7564546c3961869564d620b734864
Philippe Hamel <philippehamel85@gmail.com>
2025-03-26 17:22:15 -0400

    controller

---
+++ jokeApp/src/main/java/com/example/jokeApp/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package com.example.jokeApp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.jokeApp.services.JokeService;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+
+}
