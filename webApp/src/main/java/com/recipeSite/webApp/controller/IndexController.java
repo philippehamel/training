41c1bec41cfc257242ffbcff218250f890b83059
Philippe Hamel <philippehamel85@gmail.com>
2025-04-10 15:23:58 -0400

    populates index page with recipes

--- webApp/src/main/java/com/recipeSite/webApp/controller/IndexController.java
+++ webApp/src/main/java/com/recipeSite/webApp/controller/IndexController.java
@@ -1,14 +1,25 @@
 package com.recipeSite.webApp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.recipeSite.webApp.service.RecipeService;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping({"", "/", "/index"})
-    public String getIndexPage() {
-        return "index";
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(Model model) {
+
+        model.addAttribute("recipes", recipeService.getRecipes());
+
+        return "index";
+    }
 }
