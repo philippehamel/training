10df5f4deda3616b2e79ea092abd2afc47d24df5
Philippe Hamel <philippehamel85@gmail.com>
2025-04-12 22:51:42 -0400

    add test

---
+++ webApp/src/test/java/com/recipeSite/webApp/model/CategoryTest.java
@@ -0,0 +1,23 @@
+package com.recipeSite.webApp.model;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CategoryTest {
+
+    static Category category;
+
+    @BeforeAll
+    public static void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+        Long idValue = 4L;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
+    }
+}
